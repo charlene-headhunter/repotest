2c036af13cf435607bf32cbf6ab22f981b672821
charlene-headhunter <mbuguawamaitha1@gmail.com>
2025-02-06 01:12:49 +0300

    cypress run5

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
 
-    baseUrl: "http://localhost:8000",
+    baseUrl: "http://0.0.0.0:8000",
       requestTimeout: 20000,
   },
 });
