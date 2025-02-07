0d634770e9cbed526cfa2a762f352a9c3fd842a0
charlene-headhunter <mbuguawamaitha1@gmail.com>
2025-02-07 04:56:26 +0300

    help3

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,7 +7,7 @@ describe("Login Tests", () => {
   });
 
   it("Displays error message for invalid credentials", () => {
-    cy.get('a[normalize-space()="Log in"]', { timeout: 10000 }) // Increase timeout to 10 seconds
+    cy.contains('Log in', { timeout: 10000 }) // Increase timeout to 10 seconds
           .click();
     cy.get('#email', { timeout: 10000 }).should('be.visible');
     cy.get("#email").type("invalid@example.com");
